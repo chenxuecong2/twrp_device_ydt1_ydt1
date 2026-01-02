2e99b851e2081e39ef3d603c5fa69b8e0e9db36d
chenxuecong2 <168695974+chenxuecong2@users.noreply.github.com>
2026-01-02 17:20:52 +0800

    Happy new year

--- device.mk
+++ device.mk
@@ -1,8 +1,11 @@
 #
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 LOCAL_PATH := device/ydt1/ydt1
+
+#
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
